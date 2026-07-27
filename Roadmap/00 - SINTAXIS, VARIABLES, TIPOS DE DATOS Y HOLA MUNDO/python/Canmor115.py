e33e2b2061f561a35101ee5a759291ba4aea085d
Canmor115 <cxcmasaxa@gmail.com>
2026-07-27 19:11:31 +0200

    Reto 1 resuelto

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Canmor115.py
@@ -0,0 +1,44 @@
+# Lenguaje de programación seleccionado:
+# https://es.python.org/
+
+# Comentario de una línea
+
+"""
+Esto también es
+un comentario
+en varias líneas
+"""
+
+"""
+Y esto también es
+un comentario en varias 
+líneas
+"""
+
+# Creación de una variable y una constante
+
+my_var = "Mi variable"
+MY_CONSTANT = "Variable constante"
+
+# Variable char
+var_char = "Esto es una cadena de texto"
+var_char_2 = "Esto también es una variable de texto"
+
+# Variable entero
+var_int = 5
+# Variable float
+var_float = 5.6
+
+
+# Variable booleana
+var_bool = True
+var_bool = False
+
+print("var_char es de tipo:", type(var_char))
+print("var_char_2 es de tipo:", type(var_char_2))
+print("var_int es de tipo:", type(var_int))
+print("var_float es de tipo:", type(var_float))
+print("var_bool es de tipo:", type(var_bool))
+
+# Imprimimos por consola el mensaje:
+print("¡Hola, Python!")
